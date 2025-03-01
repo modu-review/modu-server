031fccc9eb26eeb568f928725c4e49ad8a0fa2f7
micka <bbubboru22@gmail.com>
2025-03-01 23:20:37 +0900

    project commit

---
+++ src/main/java/com/modureview/ModuReviewApplication.java
@@ -0,0 +1,13 @@
+package com.modureview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuReviewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ModuReviewApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/modureview/ModuReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modureview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuReviewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
