3697e6da487dab7e8fa0ffc12d77e36fb349f578
iseonbin <twinkey0201@gmail.com>
2025-05-18 22:26:28 +0900

    feat(BoardDetail) : BoardService.java Board의 서비스 로직입니다.

---
+++ src/main/java/com/modureview/service/BoardService.java
@@ -0,0 +1,36 @@
+package com.modureview.service;
+
+import com.modureview.dto.BoardDetailResponse;
+import com.modureview.entity.Board;
+import com.modureview.enums.BoardErrorCode;
+import com.modureview.exception.BoardCustomException;
+import com.modureview.repository.BoardRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BoardService {
+  private final BoardRepository boardRepository;
+
+  public BoardDetailResponse boardDetail(Long boardId) {
+    Board findBoard = boardRepository.findById(boardId).orElseThrow(
+        () -> new BoardCustomException(BoardErrorCode.NOT_FOUND_BOARD)
+    );
+    BoardDetailResponse response = BoardDetailResponse.builder()
+        .board_id(findBoard.getId())
+        .title(findBoard.getTitle())
+        .category(findBoard.getCategory())
+        .author(findBoard.getAuthor())
+        .create_At(findBoard.getCreatedAt())
+        .content(findBoard.getContent())
+        .comment_count(findBoard.getCommentsCount())
+        .bookmarks(findBoard.getBookmarksCount())
+        .build();
+
+    return response;
+    }
+}
