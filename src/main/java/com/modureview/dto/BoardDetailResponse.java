35f303bbdbd4220b304c760ec814bc9e2b401562
iseonbin <twinkey0201@gmail.com>
2025-05-18 22:38:02 +0900

    feat(BoardDetail) : BoardDetailResponse Response를 설정하기 위한 DTO입니다.

---
+++ src/main/java/com/modureview/dto/BoardDetailResponse.java
@@ -0,0 +1,21 @@
+package com.modureview.dto;
+
+import com.modureview.entity.Board;
+import com.modureview.entity.Category;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
+public record BoardDetailResponse(
+    Long board_id,
+    String title,
+    Category category,
+    String author,
+    LocalDateTime create_At,
+    String content,
+    Integer comment_count,
+    Integer bookmarks
+) {
+
+
+}
