bccab304980d91ddd17db833f20bbb9bf1ddfe8b
iseonbin <twinkey0201@gmail.com>
2025-05-19 22:59:44 +0900

    feat(DetailBoard) : BoardErrorCode.java ErrorCode는 상속하는 BoardErrorCode입니다.

---
+++ src/main/java/com/modureview/enums/errors/BoardErrorCode.java
@@ -0,0 +1,25 @@
+package com.modureview.enums.errors;
+
+import com.modureview.enums.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum BoardErrorCode implements ErrorCode {
+  BOARD_ID_NOTFOUND(HttpStatus.NOT_FOUND,"게시글을 찾을 수 없습니다.");
+
+  private final HttpStatus httpStatus;
+  private final String message;
+
+  BoardErrorCode(HttpStatus httpStatus, String message) {
+    this.httpStatus = httpStatus;
+    this.message = message;
+  }
+
+  @Override
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+  @Override
+  public String getMessage() {
+    return message;
+  }
+}
