015da2344cf68cf7107064094a0fc48730bdad55
iseonbin <twinkey0201@gmail.com>
2025-05-19 20:30:26 +0900

    refactor(error) : JwtErrorCode.java
    ErrorCode의 인터페이스를 상속받아 사용할수있도록 변경하였습니다.

--- src/main/java/com/modureview/enums/JwtErrorCode.java
+++ src/main/java/com/modureview/enums/JwtErrorCode.java
@@ -2,7 +2,7 @@ package com.modureview.enums;
 
 import org.springframework.http.HttpStatus;
 
-public enum JwtErrorCode {
+public enum JwtErrorCode implements ErrorCode {
 
   UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
   FORBIDDEN(HttpStatus.FORBIDDEN, "유효하지 않은 사용자입니다.");
@@ -15,10 +15,12 @@ public enum JwtErrorCode {
     this.message = message;
   }
 
+  @Override // 인터페이스 메서드 구현 명시
   public HttpStatus getHttpStatus() {
     return httpStatus;
   }
 
+  @Override // 인터페이스 메서드 구현 명시
   public String getMessage() {
     return message;
   }
