221cb30bd1201342ed0bc36c7e658f94384d5e3e
iseonbin <twinkey0201@gmail.com>
2025-05-20 21:58:00 +0900

    feat(SearchBoard) : Test입니다.

---
+++ src/test/java/com/modureview/controller/BoardSearchBoardControllerTest.java
@@ -0,0 +1,205 @@
+package com.modureview.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.modureview.dto.request.BoardSearchRequest;
+import com.modureview.entity.Board;
+import com.modureview.entity.Category;
+import com.modureview.repository.BoardSearchRepository;
+import com.modureview.service.BoardSearchService;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class BoardSearchBoardControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+  @Autowired
+  private BoardSearchService boardSearchService;
+  @Autowired
+  private BoardSearchRepository boardSearchRepository;
+  @BeforeEach
+  void setUp(){
+    List<Board> boards = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      boards.add(
+          Board.builder()
+              .title("테스트" + i)
+              .authorEmail("작성자" + i)
+              .category(Category.car)
+              .content("<p>내용 예시 " + i + "<p/>")
+              .commentsCount(i + i)
+              .bookmarksCount(i + i + i)
+              .build()
+      );
+      boards.add(
+          Board.builder()
+              .title("target_1")
+              .authorEmail("target_1")
+              .category(Category.car)
+              .content("<p> target_1 <p/>")
+              .commentsCount(24)
+              .bookmarksCount(46)
+              .build()
+      );
+      for (int j = 10; j<20 ; j++){
+        boards.add(
+            Board.builder()
+                .title("테스트" + i)
+                .authorEmail("작성자" + i)
+                .category(Category.car)
+                .content("<p>내용 예시 " + i + "<p/>")
+                .commentsCount(i + i)
+                .bookmarksCount(i + i + i)
+                .build()
+        );
+        boards.add(
+            Board.builder()
+                .title("target_2")
+                .authorEmail("target_2")
+                .category(Category.car)
+                .content("<p> target_2 <p/>")
+                .commentsCount(22)
+                .bookmarksCount(22)
+                .build()
+        );
+        boards.add(
+            Board.builder()
+                .title("target_3")
+                .authorEmail("target_4")
+                .category(Category.car)
+                .content("<p> Target_5 <p/>")
+                .commentsCount(22)
+                .bookmarksCount(22)
+                .build()
+        );
+      }
+    }
+    boardSearchRepository.saveAll(boards);
+  }
+  @AfterEach
+  void cleanUp(){
+    boardSearchRepository.deleteAll();
+  }
+
+  @Test
+  @DisplayName("GET /reviews -keyword:테스트 page:0 sort:recent 성공시 201")
+  void Search_Success_reviews_recent() throws Exception{
+    BoardSearchRequest request = BoardSearchRequest.builder()
+        .keyword("테스트")
+        .page(0)
+        .sort("recent")
+        .build();
+
+    MvcResult mvcResult = mockMvc.perform(get("/reviews")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String responseBody = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    Object jsonObject = objectMapper.readValue(responseBody, Object.class);
+    String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+
+
+    log.info("Formatted JSON Response:");
+    log.info("prettyJson == {}", prettyJson);
+  }
+
+  @Test
+  @DisplayName("GET /reviews -keyword:테스트 page:2 sort:hotcomment 성공시 201")
+  void Search_Success_reviews_hotcomment() throws Exception{
+    BoardSearchRequest request = BoardSearchRequest.builder()
+        .keyword("테스트")
+        .page(2)
+        .sort("hotcomment")
+        .build();
+
+    MvcResult mvcResult = mockMvc.perform(get("/reviews")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String responseBody = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    Object jsonObject = objectMapper.readValue(responseBody, Object.class);
+    String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+
+    log.info("Formatted JSON Response:");
+    log.info("prettyJson == {}", prettyJson);
+  }
+  @Test
+  @DisplayName("GET /reviews -keyword:테스트 page:2 sort:hotbookmark 성공시 201")
+  void Search_Success_reviews_hotbookmark() throws Exception{
+    BoardSearchRequest request = BoardSearchRequest.builder()
+        .keyword("테스트")
+        .page(2)
+        .sort("hotbookmark")
+        .build();
+
+    MvcResult mvcResult = mockMvc.perform(get("/reviews")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String responseBody = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    Object jsonObject = objectMapper.readValue(responseBody, Object.class);
+    String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+
+    log.info("Formatted JSON Response:");
+    log.info("prettyJson == {}", prettyJson);
+  }
+  @Test
+  @DisplayName("GET /reviews -keyword:테스트 page:2 sort:hotbookmark 성공시 201")
+  void Search_Content_reviews_recent() throws Exception{
+    BoardSearchRequest request = BoardSearchRequest.builder()
+        .keyword("장충동왕족발보쌈")
+        .page(2)
+        .sort("recent")
+        .build();
+
+    MvcResult mvcResult = mockMvc.perform(get("/reviews")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String responseBody = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    Object jsonObject = objectMapper.readValue(responseBody, Object.class);
+    String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+
+    log.info("Formatted JSON Response");
+    log.info("prettyJson == {}", prettyJson);
+  }
+
+}
+
---
+++ src/test/java/com/modureview/service/BoardSearchServiceTest.java
@@ -0,0 +1,140 @@
+package com.modureview.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.modureview.dto.request.BoardSearchRequest;
+import com.modureview.dto.response.BoardSearchResponse;
+import com.modureview.dto.response.CustomPageResponse;
+import com.modureview.entity.Board;
+import com.modureview.entity.Category;
+import com.modureview.repository.BoardSearchRepository;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class BoardSearchServiceTest {
+  @Autowired
+  private BoardSearchService boardSearchService;
+  @Autowired
+  private BoardSearchRepository boardSearchRepository;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @BeforeEach
+  void setUp(){
+    List<Board> boards = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      boards.add(
+          Board.builder()
+              .title("테스트" + i)
+              .authorEmail("작성자" + i)
+              .category(Category.car)
+              .content("<p>내용 예시 " + i + "<p/>")
+              .commentsCount(i + i)
+              .bookmarksCount(i + i + i)
+              .build()
+      );
+      boards.add(
+          Board.builder()
+              .title("target_1")
+      .authorEmail("target_1")
+              .category(Category.car)
+              .content("<p> target_1 <p/>")
+              .commentsCount(24)
+              .bookmarksCount(46)
+              .build()
+      );
+      for (int j = 10; j<20 ; j++){
+        boards.add(
+            Board.builder()
+                .title("테스트" + i)
+                .authorEmail("작성자" + i)
+                .category(Category.car)
+                .content("<p>내용 예시 " + i + "<p/>")
+                .commentsCount(i + i)
+                .bookmarksCount(i + i + i)
+                .build()
+        );
+        boards.add(
+            Board.builder()
+                .title("target_2")
+                .authorEmail("target_2")
+                .category(Category.car)
+                .content("<p> target_2 <p/>")
+                .commentsCount(22)
+                .bookmarksCount(22)
+                .build()
+        );
+        boards.add(
+            Board.builder()
+                .title("target_3")
+                .authorEmail("target_4")
+                .category(Category.car)
+                .content("<p> Target_5 <p/>")
+                .commentsCount(22)
+                .bookmarksCount(22)
+                .build()
+        );
+      }
+    }
+    boardSearchRepository.saveAll(boards);
+  }
+  @AfterEach
+  void cleanUp(){
+    boardSearchRepository.deleteAll();
+  }
+  @Test
+  @DisplayName("Search 성공")
+  void Search_Success_recent() throws Exception{
+
+    BoardSearchRequest request  = BoardSearchRequest.builder()
+        .keyword("target")
+        .page(1)
+        .sort("recent")
+        .build();
+    long startTime = System.nanoTime();
+    CustomPageResponse<BoardSearchResponse> rep = boardSearchService.boardSearch(request);
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime); // 나노초 단위
+    double durationMs = duration / 1_000_000.0; // 밀리초 단위
+    log.info("boardSearchServiceTest.Search_Success_recent() 실행 시간: {} ns ({} ms)", duration, String.format("%.3f", durationMs));
+    String realJson = objectMapper.
+        enable(SerializationFeature.INDENT_OUTPUT)
+        .writeValueAsString(rep);
+    log.info("realJson == {}", realJson);
+  }
+
+  @Test
+  @DisplayName("조회개수가 0개")
+  void Search_fail_recent() throws Exception{
+    BoardSearchRequest request  = BoardSearchRequest.builder()
+        .keyword("장충동왕족발보쌈")
+        .page(1)
+        .sort("recent")
+        .build();
+    long startTime = System.nanoTime();
+    CustomPageResponse<BoardSearchResponse> rep = boardSearchService.boardSearch(request);
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime); // 나노초 단위
+    double durationMs = duration / 1_000_000.0; // 밀리초 단위
+    log.info("boardSearchServiceTest.Search_fail_recent() 실행 시간: {} ns ({} ms)", duration, String.format("%.3f", durationMs));
+    String realJson = objectMapper.
+        enable(SerializationFeature.INDENT_OUTPUT)
+        .writeValueAsString(rep);
+    log.info("realJson == {}", realJson);
+
+  }
+}
