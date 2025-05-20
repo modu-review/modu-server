9afd0423600552303a94f1d2cd31cd5063d72fb8
iseonbin <twinkey0201@gmail.com>
2025-05-20 21:55:52 +0900

    feat(SearchBoard) : BoardSearchRepository.java
    "(LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
          " b.content      LIKE CONCAT('%', :keyword, '%')        OR " +
          "LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%')))")

---
+++ src/main/java/com/modureview/repository/BoardSearchRepository.java
@@ -0,0 +1,16 @@
+package com.modureview.repository;
+
+import com.modureview.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BoardSearchRepository extends JpaRepository<Board, Long> {
+  @Query("SELECT b FROM Board b WHERE " +
+      "(LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+      " b.content      LIKE CONCAT('%', :keyword, '%')        OR " +
+      "LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+  Page<Board> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
+}
