68493786542ad2cb0c8aaf4a3ff27aaed2feed34
yellTa <32352161+yellTa@users.noreply.github.com>
2025-05-21 19:51:13 +0900

    feat(게시글저장) : presignedUrl, uuid 프론트에 전달하는 로직 구성
    BoardController.java=====
    Uuid, PresignedUrl을 만들어 return하는 컨트롤러
    
    PresignRequest.java====
    front에서 들어오는 requestDto
    
    # ################
    # <타입> (scope): <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    #
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    #
    # ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    #
    # ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # ################

--- src/main/java/com/modureview/controller/BoardController.java
+++ src/main/java/com/modureview/controller/BoardController.java
@@ -1,10 +1,16 @@
 package com.modureview.controller;
 
 import com.modureview.dto.BoardDetailResponse;
+import com.modureview.dto.request.PresignRequest;
 import com.modureview.service.BoardService;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +24,18 @@ public class BoardController {
       @RequestParam Long board_id
   ){
     return ResponseEntity.ok().body(boardService.boardDetail(board_id));
+  }
+
+  @PostMapping("/presign")
+  public ResponseEntity<?> createImageInfo(@RequestBody PresignRequest fileType) {
+    String type = fileType.fileType();
+    String imageUuid = boardService.createImageID();
+    String presignedURL = boardService.createPresignedURL(imageUuid + "." + type);
+
+    Map<String, String> result = new HashMap<>();
+    result.put("uuid", imageUuid);
+    result.put("presignedUrl", presignedURL);
+    return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
 }
