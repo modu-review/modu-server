6e551bc920bbc4cea3543ad55655e8aaa1610017
iseonbin <twinkey0201@gmail.com>
2025-06-01 19:28:36 +0900

    refactor(Category) : BoardSearchCategory.java
    기존 SQL 도 되지만 countQuery에서 성능 향상을 위해서 주석처리해놨습니다.

--- src/main/java/com/modureview/repository/BoardSearchRepository.java
+++ src/main/java/com/modureview/repository/BoardSearchRepository.java
@@ -8,9 +8,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BoardSearchRepository extends JpaRepository<Board, Long> {
-  @Query("SELECT b FROM Board b WHERE " +
-      "(LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-      " b.content      LIKE CONCAT('%', :keyword, '%')        OR " +
-      "LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+
+  @Query(
+      value = """
+            SELECT b
+            FROM Board b
+            WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+               OR b.content      LIKE CONCAT('%', :keyword, '%')
+               OR LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%'))
+          """
+      /*,
+      countQuery = """
+            SELECT COUNT(b)
+            FROM Board b
+            WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+               OR b.content      LIKE CONCAT('%', :keyword, '%')
+               OR LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%'))
+          """*/
+  )
   Page<Board> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
