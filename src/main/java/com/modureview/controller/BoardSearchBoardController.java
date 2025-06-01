7efd1144c9feb73088bf866462bf01e1e7d11700
iseonbin <twinkey0201@gmail.com>
2025-06-01 19:30:19 +0900

    refactor(Category) : BoardSearchBoardController.java
    keyword를 디코딩하였습니다.

--- src/main/java/com/modureview/controller/BoardSearchBoardController.java
+++ src/main/java/com/modureview/controller/BoardSearchBoardController.java
@@ -1,22 +1,21 @@
 package com.modureview.controller;
 
-import com.modureview.dto.request.BoardCategoryRequest;
-import com.modureview.dto.request.BoardSearchRequest;
 import com.modureview.dto.response.BoardSearchResponse;
 import com.modureview.dto.response.CustomPageResponse;
-import com.modureview.dto.response.CustomSlicePageResponse;
 import com.modureview.entity.Board;
 import com.modureview.service.BoardSearchService;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BoardSearchBoardController {
@@ -29,8 +28,13 @@ public class BoardSearchBoardController {
       @RequestParam(name = "keyword") String keyword,
       @RequestParam(name = "page", defaultValue = "0") int page,
       @RequestParam(name = "sort", defaultValue = "recent") String sort
-  ) {
-    Page<Board> boardPage = boardSearchService.boardSearch(keyword, page, sort);
+  ) throws UnsupportedEncodingException {
+
+    log.info("keyword == {}", keyword);
+    String decodeKeyword = URLDecoder.decode(keyword, "UTF-8");
+    log.info("decodeKeyword == {}", decodeKeyword);
+    log.info("sort == {}", sort);
+    Page<Board> boardPage = boardSearchService.boardSearch(decodeKeyword, page, sort);
     List<BoardSearchResponse> listSearchBoard = boardPage.getContent().stream()
         .map(BoardSearchResponse::fromEntity)
         .toList();
