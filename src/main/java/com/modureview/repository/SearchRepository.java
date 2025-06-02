98e55a8f0c8ba0b7a983d17f0d5b23df2ef69b21
iseonbin <twinkey0201@gmail.com>
2025-06-02 14:34:57 +0900

    refactor(SearchBoard) : SearchRepository.java
    주석을 해제하였습니다.

--- src/main/java/com/modureview/repository/SearchRepository.java
+++ src/main/java/com/modureview/repository/SearchRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface BoardSearchRepository extends JpaRepository<Board, Long> {
+public interface SearchRepository extends JpaRepository<Board, Long> {
 
   @Query(
       value = """
@@ -17,14 +17,14 @@ public interface BoardSearchRepository extends JpaRepository<Board, Long> {
                OR b.content      LIKE CONCAT('%', :keyword, '%')
                OR LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%'))
           """
-      /*,
+      ,
       countQuery = """
             SELECT COUNT(b)
             FROM Board b
             WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
                OR b.content      LIKE CONCAT('%', :keyword, '%')
                OR LOWER(b.authorEmail) LIKE LOWER(CONCAT('%', :keyword, '%'))
-          """*/
+          """
   )
   Page<Board> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
