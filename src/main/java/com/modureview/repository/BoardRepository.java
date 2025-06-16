fda0aa7fb26dd26e34b71a96fcd024960bf559d5
yellTa <32352161+yellTa@users.noreply.github.com>
2025-06-17 00:13:10 +0900

    feat(Best후기 집계) : 카테고리별 top 6 데이터 가져오기
    BoardRepository.java=====
    카테고리별 top6데이터를 가져오는 쿼리문 작성
    
    BoardReviewsService.java=====
    집계 서비스에서 Top6데이터를 가져오는 로직 사용

--- src/main/java/com/modureview/repository/BoardRepository.java
+++ src/main/java/com/modureview/repository/BoardRepository.java
@@ -1,8 +1,27 @@
 package com.modureview.repository;
 
 import com.modureview.entity.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
+  @Query(
+      value = """
+            WITH RankedBoards AS (
+                SELECT
+                    b.*,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY b.category
+                        ORDER BY (b.bookmarks_count * 4 + b.view_count + b.comments_count * 2) DESC
+                    ) AS rn
+                FROM
+                    board b
+            )
+            SELECT *
+            FROM RankedBoards
+            WHERE rn <= 6
+        """,
+      nativeQuery = true)
+  List<Board> findTop6BoardsPerCategory();
 }
