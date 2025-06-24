0925fdcc86b37005d9cda5095c8f885fe7e0a4f8
yellTa <32352161+yellTa@users.noreply.github.com>
2025-06-24 22:07:25 +0900

    feat(Best후기 집계) : mysql, redis에러코드 지정

--- src/main/java/com/modureview/enums/errors/BestReviewErrorCode.java
+++ src/main/java/com/modureview/enums/errors/BestReviewErrorCode.java
@@ -4,7 +4,9 @@ import com.modureview.enums.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum BestReviewErrorCode implements ErrorCode {
-  JSON_PROCESSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JSON파싱중 에러가 발생했습니다.");
+  JSON_PROCESSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JSON파싱중 에러가 발생했습니다."),
+  MYSQL_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Mysql Server에 연겷할 수 없습니다."),
+  REDIS_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Redis Server에 연결할 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
