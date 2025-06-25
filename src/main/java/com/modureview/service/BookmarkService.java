edd3fbcdbb057de0006924238e6d131398c31026
yellTa <32352161+yellTa@users.noreply.github.com>
2025-06-25 13:02:25 +0900

    feat(북마크 추가) : redis에 값이 있는 경우 값 추가

--- src/main/java/com/modureview/service/BookmarkService.java
+++ src/main/java/com/modureview/service/BookmarkService.java
@@ -11,13 +11,9 @@ public class BookmarkService {
   private final StringRedisTemplate stringRedisTemplate;
 
   public void redisUpdate(Long boardId) {
-    /*
-    Todo :1. Redis에 id있나 체크
-    * */
     String redisKey = "board:" + boardId;
     if (stringRedisTemplate.hasKey(redisKey)){
-      // TODO 2. if redis has the board id then update the score + 4 else skip
-
+      stringRedisTemplate.opsForValue().increment(redisKey, 4);
     }
   }
 
