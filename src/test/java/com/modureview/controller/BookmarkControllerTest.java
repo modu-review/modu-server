3f871d885c057136db806b661acf28e31169de1b
yellTa <32352161+yellTa@users.noreply.github.com>
2025-06-30 20:44:00 +0900

    test(북마크제거) : Spring애플리케이션을 실행해서 북마크 제거 로직 수행

--- src/test/java/com/modureview/controller/BookmarkControllerTest.java
+++ src/test/java/com/modureview/controller/BookmarkControllerTest.java
@@ -6,6 +6,7 @@ import com.modureview.dto.request.BookmarkRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Step;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,10 +18,20 @@ class BookmarkControllerTest {
   private BookmarkController bookmarkController;
 
   @Test
-  @DisplayName("redis key가 있는 경우")
-  void redisKeyExist(){
-    BookmarkRequest request= new BookmarkRequest(1L,1L);
-    bookmarkController.updateBookmark(request);
+  @DisplayName("북마크 제거")
+  void deleteBookmark(){
+    BookmarkRequest request= new BookmarkRequest("user1@example.com");
+    bookmarkController.deleteBookmark(1L,request);
   }
+
+  @Test
+  @DisplayName("북마크추가")
+  void addBookmark(){
+    BookmarkRequest request= new BookmarkRequest("user1@example.com");
+    bookmarkController.updateBookmark(999L, request);
+
+  }
+
+
 
 }
