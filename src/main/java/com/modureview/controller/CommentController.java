f1f17e1629b5028cc35a92c00656db97bc275c7d
iseonbin <twinkey0201@gmail.com>
2025-07-02 16:04:18 +0900

    fix(CommentList) : commentcount 를 위한 재 설정 입니다.

--- src/main/java/com/modureview/controller/CommentController.java
+++ src/main/java/com/modureview/controller/CommentController.java
@@ -1,7 +1,7 @@
 package com.modureview.controller;
 
 import com.modureview.dto.response.CommentDetailResponse;
-import com.modureview.dto.response.CustomPageResponse;
+import com.modureview.dto.response.CommentListResponse;
 import com.modureview.entity.Comment;
 import com.modureview.service.CommentService;
 import java.util.List;
@@ -20,20 +20,26 @@ public class CommentController {
   private final CommentService commentService;
 
   @GetMapping("/reviews/{reviewId}/comments")
-  public ResponseEntity<CustomPageResponse<CommentDetailResponse>> getCommentList(
+  public ResponseEntity<CommentListResponse> getCommentList(
       @PathVariable Long reviewId,
-      @RequestParam(name = "page", defaultValue = "0") int page
+      @RequestParam(name = "page", defaultValue = "1") int page  // 페이지 기본값을 1로 바꿔도 좋습니다
   ) {
     Page<Comment> commentPage = commentService.commentList(reviewId, page);
     List<CommentDetailResponse> listComment = commentPage.getContent().stream()
         .map(CommentDetailResponse::fromEntity)
         .toList();
-    CustomPageResponse<CommentDetailResponse> commentPageResponse = new CustomPageResponse<>(
-        listComment,
-        commentPage.getNumber() + 1,
-        commentPage.getTotalPages()
-    );
-    return ResponseEntity.ok(commentPageResponse);
+    Integer rawCount = commentService.commentCount(reviewId);
+    int commentsCount = rawCount != null ? rawCount : 0;
+    int totalPages = commentPage.getTotalPages();
+    int currentPage = (totalPages == 0 ? 0 : commentPage.getNumber() + 1);
+    CommentListResponse response = CommentListResponse.builder()
+        .commentsCount(commentsCount)
+        .comments(listComment)
+        .currentPage(currentPage)
+        .totalPages(totalPages)
+        .build();
+
+    return ResponseEntity.ok(response);
   }
 
 }
