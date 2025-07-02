734e9111554219abaf043d4a571856a50186b41c
iseonbin <twinkey0201@gmail.com>
2025-07-02 16:04:04 +0900

    fix(CommentList) : count 를 따로 발급받아서 comment 조회할때 다시 넣었습니다.

--- src/main/java/com/modureview/service/CommentService.java
+++ src/main/java/com/modureview/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.modureview.service;
 
 import com.modureview.entity.Comment;
+import com.modureview.repository.BoardRepository;
 import com.modureview.repository.CommentRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,15 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
   private final CommentRepository commentRepository;
+  private final BoardRepository boardRepository;
 
   public Page<Comment> commentList(Long boardId, int Page) {
     Pageable pageable = PageRequest.of(Page - 1, 12, Sort.by(Direction.DESC, "createdAt"));
 
     return commentRepository.findByBoardId(boardId, pageable);
+  }
+
+  public Integer commentCount(Long boardId) {
+    return boardRepository.findCommentsCountById(boardId);
   }
 }
