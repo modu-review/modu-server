03f9f4c4a3995127ce60d0e871cd9eaaff8bc2bf
iseonbin <twinkey0201@gmail.com>
2025-07-02 21:59:14 +0900

    왜인지 모르겠는 파일 이름명 변경입니다.

--- src/main/java/com/modureview/controller/BookmarkController.java
+++ src/main/java/com/modureview/controller/BookmarkController.java
@@ -1,6 +1,7 @@
 package com.modureview.controller;
 
 import com.modureview.dto.request.BookmarkRequest;
+import com.modureview.dto.response.BookMarkDetailResponse;
 import com.modureview.service.BoardService;
 import com.modureview.service.BookMarkService;
 import com.modureview.service.UserService;
@@ -8,17 +9,28 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
-public class BookmarkController {
+public class BookMarkController {
 
   private final BoardService boardService;
   private final BookMarkService bookmarkService;
   private final UserService userService;
+
+  @GetMapping("/reviews/{reviewId}/bookmarks")
+  public ResponseEntity<BookMarkDetailResponse> getBookMarkDetail(
+      @PathVariable Long reviewId,
+      @CookieValue(name = "email", required = false, defaultValue = "null") String email) {
+    BookMarkDetailResponse bookMarkDetailResponse = bookmarkService.bookMarkDetail(reviewId, email);
+    return ResponseEntity.ok(bookMarkDetailResponse);
+
+  }
 
   @PostMapping("reviews/{reviewId}/bookmark")
   public ResponseEntity<?> updateBookmark(@PathVariable Long reviewId,
@@ -39,4 +51,5 @@ public class BookmarkController {
 
     return new ResponseEntity<>(HttpStatus.OK);
   }
+
 }
