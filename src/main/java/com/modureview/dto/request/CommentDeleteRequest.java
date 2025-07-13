b561b10a98eb4dd66d08fe403e10d78f12d687ab
iseonbin <twinkey0201@gmail.com>
2025-07-13 17:00:35 +0900

    fix(CommentDeleteRequest) : 댓글 삭제 요청 본문에 boardId 제거
    본문에 BoardID를 삭제하였습니다.

--- src/main/java/com/modureview/dto/request/CommentDeleteRequest.java
+++ src/main/java/com/modureview/dto/request/CommentDeleteRequest.java
@@ -2,7 +2,7 @@ package com.modureview.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record CommentDeleteRequest(@JsonProperty("user_email") String userEmail, @JsonProperty("board_id") Long boardId,
-                                  @JsonProperty("comment_id") Long commentId) {
+public record CommentDeleteRequest(@JsonProperty("user_email") String userEmail,
+								   @JsonProperty("comment_id") Long commentId) {
 
 }
