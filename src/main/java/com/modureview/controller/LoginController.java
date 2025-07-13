4cf1bfe8b42919814f4b16788e27387756e3e26d
iseonbin <twinkey0201@gmail.com>
2025-07-13 16:59:58 +0900

    fix(LoginController) : URL 엔드포인트 변경
    /login/result -> /user/login
    /Logout -> /user/logout

--- src/main/java/com/modureview/controller/LoginController.java
+++ src/main/java/com/modureview/controller/LoginController.java
@@ -1,16 +1,8 @@
 package com.modureview.controller;
 
-import com.modureview.enums.errors.JwtErrorCode;
-import com.modureview.exception.jwtError.InvalidTokenException;
-import com.modureview.service.JwtTokenService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -18,58 +10,69 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.modureview.enums.errors.JwtErrorCode;
+import com.modureview.exception.jwtError.InvalidTokenException;
+import com.modureview.service.JwtTokenService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
 
-  private final JwtTokenService jwtTokenService;
+	private final JwtTokenService jwtTokenService;
 
-  @GetMapping("/login/result")
-  public ResponseEntity<String> tokenIssue(@RequestParam String email,
-      HttpServletResponse response) {
-    List<ResponseCookie> responseCookies = jwtTokenService.loginTokenIssue(email);
+	@GetMapping("/user/login")
+	public ResponseEntity<String> tokenIssue(@RequestParam String email,
+		HttpServletResponse response) {
+		List<ResponseCookie> responseCookies = jwtTokenService.loginTokenIssue(email);
 
-    responseCookies.stream()
-        .forEach(cookie -> response.addHeader("Set-Cookie", cookie.toString()));
-    log.info("토큰 발행 완료");
-    return ResponseEntity.ok("토큰 발행 완료 : " + email);
-  }
+		responseCookies.stream()
+			.forEach(cookie -> response.addHeader("Set-Cookie", cookie.toString()));
+		log.info("토큰 발행 완료");
+		return ResponseEntity.ok("토큰 발행 완료 : " + email);
+	}
 
-  @GetMapping("/token/refresh")
-  public ResponseEntity<?> refresh(HttpServletRequest request,
-      HttpServletResponse response) {
-    String refreshToken = jwtTokenService.extractCookie(request, "refreshToken")
-        .orElseThrow(() -> new InvalidTokenException(JwtErrorCode.UNAUTHORIZED));
+	@GetMapping("/token/refresh")
+	public ResponseEntity<?> refresh(HttpServletRequest request,
+		HttpServletResponse response) {
+		String refreshToken = jwtTokenService.extractCookie(request, "refreshToken")
+			.orElseThrow(() -> new InvalidTokenException(JwtErrorCode.UNAUTHORIZED));
 
-    ResponseCookie newAccessToken = jwtTokenService.reIssueAccessToken(jwtTokenService.extractSubject(refreshToken));
+		ResponseCookie newAccessToken = jwtTokenService.reIssueAccessToken(
+			jwtTokenService.extractSubject(refreshToken));
 
-    response.addHeader("Set-Cookie", newAccessToken.toString());
+		response.addHeader("Set-Cookie", newAccessToken.toString());
 
-    return ResponseEntity.ok().build();
-  }
+		return ResponseEntity.ok().build();
+	}
 
-  @GetMapping("/logout")
-  public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response){
-    HttpHeaders headers = new HttpHeaders();
+	@GetMapping("/user/logout")
+	public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+		HttpHeaders headers = new HttpHeaders();
 
-    Cookie[] cookies = request.getCookies();
+		Cookie[] cookies = request.getCookies();
 
-    if (cookies != null) {
-      Arrays.stream(cookies).forEach(cookie -> {
-        if (cookie.getName().equals("accessToken") ||
-            cookie.getName().equals("refreshToken") ||
-            cookie.getName().equals("userEmail")) {
+		if (cookies != null) {
+			Arrays.stream(cookies).forEach(cookie -> {
+				if (cookie.getName().equals("accessToken") ||
+					cookie.getName().equals("refreshToken") ||
+					cookie.getName().equals("userEmail")) {
 
-          ResponseCookie expiredCookie = jwtTokenService.expireCookie(
-              ResponseCookie.from(cookie.getName(), "").build()
-          );
-          headers.add(HttpHeaders.SET_COOKIE, expiredCookie.toString());
-          log.info("Expired cookie: {}", cookie.getName());
-        }
-      });
-    }
-    return ResponseEntity.ok().headers(headers).body("Logged out successfully.");
+					ResponseCookie expiredCookie = jwtTokenService.expireCookie(
+						ResponseCookie.from(cookie.getName(), "").build()
+					);
+					headers.add(HttpHeaders.SET_COOKIE, expiredCookie.toString());
+					log.info("Expired cookie: {}", cookie.getName());
+				}
+			});
+		}
+		return ResponseEntity.ok().headers(headers).body("Logged out successfully.");
 
-  }
+	}
 }
