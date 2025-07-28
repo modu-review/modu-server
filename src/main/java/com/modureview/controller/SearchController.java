7946389c4a1e7e76e94d2cfc627867740cb197c6
yellTa <32352161+yellTa@users.noreply.github.com>
2025-07-28 22:55:32 +0900

    rebase

--- src/main/java/com/modureview/controller/SearchController.java
+++ src/main/java/com/modureview/controller/SearchController.java
@@ -1,10 +1,17 @@
 package com.modureview.controller;
 
+import com.modureview.dto.response.BoardSearchResponse;
+import com.modureview.dto.response.CustomPageResponse;
+import com.modureview.dto.response.CustomSlicePageResponse;
+import com.modureview.entity.Board;
+import com.modureview.entity.Category;
+import com.modureview.service.SearchService;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
@@ -12,70 +19,63 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.modureview.dto.response.CustomPageResponse;
-import com.modureview.dto.response.CustomSlicePageResponse;
-import com.modureview.dto.response.SliceBoardResponse;
-import com.modureview.entity.Board;
-import com.modureview.entity.Category;
-import com.modureview.service.SearchService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class SearchController {
 
-	private final SearchService searchService;
+  private final SearchService searchService;
 
-	@GetMapping("/search")
-	public ResponseEntity<CustomPageResponse<SliceBoardResponse>> getBoardSearch(
-		@RequestParam(name = "keyword") String keyword,
-		@RequestParam(name = "page", defaultValue = "0") int page,
-		@RequestParam(name = "sort", defaultValue = "recent") String sort
-	) throws UnsupportedEncodingException {
-		String decodeKeyword = URLDecoder.decode(keyword, "UTF-8");
-		log.info("Searching for " + decodeKeyword);
-		Page<Board> boardPage = searchService.boardSearch(decodeKeyword, page, sort);
-		List<SliceBoardResponse> listSearchBoard = boardPage.getContent().stream()
-			.map(SliceBoardResponse::fromEntity)
-			.toList();
-		CustomPageResponse<SliceBoardResponse> SearchPage = new CustomPageResponse<>(
-			listSearchBoard,
-			boardPage.getNumber() + 1,
-			boardPage.getTotalPages()
-		);
 
-		return ResponseEntity.ok().body(SearchPage);
-	}
+  @GetMapping("/reviews")
+  public ResponseEntity<CustomPageResponse<BoardSearchResponse>> getBoardSearch(
+      @RequestParam(name = "keyword") String keyword,
+      @RequestParam(name = "page", defaultValue = "0") int page,
+      @RequestParam(name = "sort", defaultValue = "recent") String sort
+  ) throws UnsupportedEncodingException {
+    String decodeKeyword = URLDecoder.decode(keyword, "UTF-8");
+    log.info("Searching for " + decodeKeyword);
+    Page<Board> boardPage = searchService.boardSearch(decodeKeyword, page, sort);
+    List<BoardSearchResponse> listSearchBoard = boardPage.getContent().stream()
+        .map(BoardSearchResponse::fromEntity)
+        .toList();
+    CustomPageResponse<BoardSearchResponse> SearchPage = new CustomPageResponse<>(
+        listSearchBoard,
+        boardPage.getNumber() + 1,
+        boardPage.getTotalPages()
+    );
 
-	@GetMapping("/reviews")
-	public ResponseEntity<CustomSlicePageResponse<SliceBoardResponse>> getBoardsByCategory(
-		@RequestParam(name = "categoryId") Category category,
-		@RequestParam(name = "cursor", defaultValue = "0") Long cursorId,
-		@RequestParam(name = "sort", defaultValue = "recent") String sort) {
-		Slice<Board> boardSlice = searchService.getCategoryBoard(category, cursorId, sort);
-		List<SliceBoardResponse> dtoList = boardSlice.getContent().stream()
-			.map(SliceBoardResponse::fromEntity)
-			.collect(Collectors.toList());
+    return ResponseEntity.ok().body(SearchPage);
+  }
 
-		Long nextCursorValue = null;
-		if (boardSlice.hasNext() && !boardSlice.getContent().isEmpty()) {
-			Board lastBoardInSlice = boardSlice.getContent().get(boardSlice.getContent().size() - 1);
-			nextCursorValue = lastBoardInSlice.getId();
-		}
 
-		CustomSlicePageResponse<SliceBoardResponse> customResponse = new CustomSlicePageResponse<>(
-			dtoList,
-			nextCursorValue,
-			boardSlice.hasNext(),
-			boardSlice.getNumberOfElements(),
-			boardSlice.getSize(),
-			boardSlice.isFirst()
-		);
+  @GetMapping("/reviews")
+  public ResponseEntity<CustomSlicePageResponse<BoardSearchResponse>> getBoardsByCategory(
+      @RequestParam(name = "categoryId") Category category,
+      @RequestParam(name = "cursor", defaultValue = "0") Long cursorId,
+      @RequestParam(name = "sort", defaultValue = "recent") String sort) {
+    Slice<Board> boardSlice = searchService.getCategoryBoard(category, cursorId, sort);
+    List<BoardSearchResponse> dtoList = boardSlice.getContent().stream()
+        .map(BoardSearchResponse::fromEntity)
+        .collect(Collectors.toList());
 
-		return ResponseEntity.ok(customResponse);
-	}
+    Long nextCursorValue = null;
+    if (boardSlice.hasNext() && !boardSlice.getContent().isEmpty()) {
+      Board lastBoardInSlice = boardSlice.getContent().get(boardSlice.getContent().size() - 1);
+      nextCursorValue = lastBoardInSlice.getId();
+    }
+
+    CustomSlicePageResponse<BoardSearchResponse> customResponse = new CustomSlicePageResponse<>(
+        dtoList,
+        nextCursorValue,
+        boardSlice.hasNext(),
+        boardSlice.getNumberOfElements(),
+        boardSlice.getSize(),
+        boardSlice.isFirst()
+    );
+
+    return ResponseEntity.ok(customResponse);
+  }
+
 
 }
