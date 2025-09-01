4aac7aba55babcfa73841ec7aab362495c27123e
iseonbin <twinkey0201@gmail.com>
2025-09-01 09:28:34 +0900

    <hotfix> :: MainService : 최신순 수정

--- src/main/java/com/modureview/repository/BoardRepository.java
+++ src/main/java/com/modureview/repository/BoardRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-  List<Board> findTop6ByOrderByCreatedAtAsc();
+  List<Board> findTop6ByOrderByCreatedAtDesc();
 
   @Query(value =
       "WITH RankedBoards AS (" +
